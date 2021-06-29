48e8980f7b7225021fb650b1774e58836570d8e6
Deepak Bhagat <bhagatd585@gmail.com>
2021-06-29 17:08:24 +0530

    add or remove roles

---
+++ commands/moderation/giveRole.js
@@ -0,0 +1,32 @@
+module.exports = {
+    commands: ['give', 'add',],
+    expectedArgs: `<mention user or user id>, <role id or role name>`,
+    minArgs: 2,
+    maxArgs: 2,
+    callback: (message, args, text, Discord) =>{
+        message.delete()
+        let userByMention = message.mentions.members.first()
+        let role = args[1]
+        let roleByName= message.guild.roles.cache.find(r => r.name === role)
+        let roleById = message.guild.roles.cache.find(r => r.id === role)
+        let userById = message.guild.members.cache.find(member => member.id === args[0])
+        if(roleByName === undefined && roleById === undefined){
+            message.channel.send('enter valid name or id')
+        }else if(roleByName === undefined && roleById !== undefined){
+            giveRole(roleById)
+        }else if(roleById === undefined && roleByName !== undefined){
+            giveRole(roleByName)
+        }
+
+       function giveRole(role){
+           if(userByMention !== undefined){
+               userByMention.roles.add(role)
+           }else if(userByMention === undefined && userById !== undefined){
+            userById.roles.add(role)
+           }else if(userById === undefined){
+               return message.channel.send('user is not in this guild or does not exist')
+           }
+       }
+    },
+    permissions: ['MANAGE_ROLES']
+}
